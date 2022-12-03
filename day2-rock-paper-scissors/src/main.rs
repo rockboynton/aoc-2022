e42bf1a8a427d2c53499336ab0dba01808ee047d
Rock Boynton <rboynton@tesla.com>
2022-12-02 23:44:52 -0800

    terrible rushed solution to day 2 part 2

--- day2-rock-paper-scissors/src/main.rs
+++ day2-rock-paper-scissors/src/main.rs
@@ -7,10 +7,11 @@ use itertools::Itertools;
 #[derive(Hash)]
 #[derive(Eq)]
 #[derive(PartialEq)]
+#[derive(Clone)]
 enum Shape {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
 }
 
 #[derive(Hash)]
@@ -52,7 +53,17 @@ fn main() {
         (Shape::Scissors, Shape::Rock),
     ]);
 
-    let mut total_score: u64 = 0;
+    let total_score = part_1(&strategy_guide, &winners);
+    println!("Total score for part 1 according to strategy: {total_score}");
+
+    let total_score = part_2(&strategy_guide, &winners);
+    println!("Total score for part 2 according to strategy: {total_score}");
+}
+
+/// For part 1, the second column is what you should play in response: X for Rock, Y for
+/// Paper, and Z for Scissors.
+fn part_1(strategy_guide: &str, winners: &HashMap<Shape, Shape>) -> u64 {
+    let mut total_score = 0;
     for game in strategy_guide.lines() {
         let (opponent_hand, my_hand) = &game.split(' ')
             .collect::<Vec<&str>>()
@@ -70,5 +81,39 @@ fn main() {
         }
     }
 
-    println!("Total score according to strategy: {total_score}");
+    total_score
+}
+
+/// For the part 2, the second column is as follows: X means you need to lose, Y means
+/// you need to end the round in a draw, and Z means you need to win.
+fn part_2(strategy_guide: &str, winners: &HashMap<Shape, Shape>) -> u64 {
+    let losers: HashMap<Shape, Shape> = HashMap::from([
+        (Shape::Paper, Shape::Rock),
+        (Shape::Scissors, Shape::Paper),
+        (Shape::Rock, Shape::Scissors),
+    ]);
+
+    let mut total_score = 0;
+    for game in strategy_guide.lines() {
+        let (opponent_hand, my_hand) = &game.split(' ')
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|s| Hand::new(s))
+            .collect_tuple()
+            .unwrap();
+
+        let score = match my_hand.shape {
+            // lose
+            Shape::Rock => losers[&opponent_hand.shape].clone() as u64,
+            // draw
+            Shape::Paper => 3 + opponent_hand.score as u64,
+            // win
+            Shape::Scissors => 6 + winners[&opponent_hand.shape].clone() as u64,
+        };
+        total_score += score;
+        // correct answer: 12989
+    }
+
+    // 11319 is wrong
+    total_score
 }
