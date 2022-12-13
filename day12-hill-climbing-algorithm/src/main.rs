c95bc84639e3b53b1236ccd969c34b6f47e92f29
Rock Boynton <boyntonrl@msoe.edu>
2022-12-13 09:48:55 -0800

    day 12 - part 1 solved

--- day12-hill-climbing-algorithm/src/main.rs
+++ day12-hill-climbing-algorithm/src/main.rs
@@ -1,7 +1,46 @@
 use std::env;
 use std::fs;
 
-type Heightmap = Vec<Vec<char>>;
+use pathfinding::prelude::astar;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct HeightPos {
+    x: i32,
+    y: i32,
+    height: char
+}
+
+impl HeightPos {
+    fn new(x: i32, y: i32, heightmap: &[Vec<char>]) -> Option<Self> {
+        Some(Self {
+            x,
+            y,
+            height: *heightmap.get(x as usize)?.get(y as usize)?
+        })
+    }
+
+    fn distance(&self, other: &HeightPos) -> u32 {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+
+    fn set_height(&mut self, height: char) {
+        self.height = height;
+    }
+
+    fn successors(&self, heightmap: &[Vec<char>]) -> Vec<(HeightPos, u32)> {
+        let &HeightPos { x, y, .. } = self;
+
+        vec![
+            HeightPos::new(x + 1, y, heightmap),
+            HeightPos::new(x, y + 1, heightmap),
+            HeightPos::new(x - 1, y, heightmap),
+            HeightPos::new(x, y - 1, heightmap)
+        ].into_iter()
+            .flatten()
+            .filter(|pos| (pos.height as u32) < (self.height as u32) + 2)
+            .map(|p| (p, 1)).collect()
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,32 +55,40 @@ fn main() {
     // println!("solution to part 2: {part2}");
 }
 
-fn solve_part1(input: &str) -> i32 {
-    // let heightmap = heightmap.lines()
-    //     .map(|line| {
-    //         line.chars()
-    //             .collect::<Vec<char>>()
-    //     })
-    //     .collect::<Heightmap>();
-
-    let mut start: (usize, usize);
-    let mut end: (usize, usize);
-
-    let heightmap = Heightmap::new();
-    for (y, row) in input.lines().enumerate() {
-        let row = heightmap[y];
-        for (x, height) in row.chars().enumerate() {
-            match height {
-                'S' => start = (y, x),
-                'E' => end = (y, x),
-                _ => ()
-            }
-            if height == 'S' {
-                start = (y, x);
-            }
+fn solve_part1(input: &str) -> usize {
+    let mut heightmap= Vec::new();
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+    for (row_idx, line) in input.lines().enumerate() {
+        let mut row = Vec::new();
+        for (col_idx, c) in line.chars().enumerate() {
+            row.push(match c {
+                'S' => {
+                    start = (col_idx as i32, row_idx as i32);
+                    'a'
+                },
+                'E' => {
+                    end = (col_idx as i32, row_idx as i32);
+                    'z'
+                },
+                height => height
+            });
         }
+        heightmap.push(row);
     }
-    1
+
+    let mut start = HeightPos::new(start.1, start.0, &heightmap).unwrap();
+    start.set_height('a');
+    let mut end = HeightPos::new(end.1, end.0, &heightmap).unwrap();
+    end.set_height('z');
+
+    let shortest_path = astar(
+        &start,
+        |pos| pos.successors(&heightmap),
+        |pos| pos.distance(&end),
+        |pos| *pos == end).unwrap();
+
+    shortest_path.1 as usize
 }
 
 #[cfg(test)]
@@ -53,6 +100,6 @@ mod tests {
         let input = include_str!("../example.txt");
 
         let part1 = solve_part1(&input.to_string());
-        assert_eq!(part1, 13140);
+        assert_eq!(part1, 31);
     }
 }
